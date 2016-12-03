c316103256a5972beae73bea1bd39544b334261c
hristy93 <hristy93@gmail.com>
2016-12-03 11:20:50 +0200

    The crawler is working and added some websites to crawl

---
+++ VkusotiikiCrawler/Ingredient.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkusotiikiCrawler
+{
+    class Ingredient
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("quantity")]
+        public string Quantity { get; set; }
+        [JsonProperty("unit")]
+        public string Unit { get; set; }
+        [JsonProperty("unstructured_data")]
+        public string UnstructuredData { get; set; }
+
+        public Ingredient()
+        {
+
+        }
+
+        public Ingredient(string ingredients)
+        {
+            var splittedIngredients = ingredients.Split(' ');
+            if (splittedIngredients.Count() == 3)
+            {
+                Quantity = splittedIngredients[0];
+                Unit = splittedIngredients[1];
+                Name = splittedIngredients[2];
+            }
+            else
+            {
+                Quantity = splittedIngredients[0];
+                Unit = splittedIngredients[1];
+                string ingredientName = "";
+                for (int i = 2; i < splittedIngredients.Count(); i++)
+                {
+                    ingredientName += splittedIngredients[i] + " ";
+                }
+                Name = ingredientName;
+            }
+
+            UnstructuredData = ingredients;
+        }
+    }
+}
---
+++ VkusotiikiCrawler/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VkusotiikiCrawler
+{
+    class Recipe
+    {
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("instructions")]
+        public string Instructions { get; set; }
+        [JsonProperty("duration")]
+        public string Duration { get; set; }
+        [JsonProperty("ingredients")]
+        public List<Ingredient> Ingredients { get; set; }
+
+        public Recipe()
+        {
+            Ingredients = new List<Ingredient>();
+        }
+    }
+}
